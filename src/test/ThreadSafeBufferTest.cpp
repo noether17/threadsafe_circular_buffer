a9165b50fa3b5c3ee38b90b82a5e6a2c162599e2
Jonathan White <jawh@umich.edu>
2024-10-12 15:37:35 -0600

    Added test for single writer thread and multiple reader threads.

--- src/test/ThreadSafeBufferTest.cpp
+++ src/test/ThreadSafeBufferTest.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
+#include <mutex>
 #include <thread>
 #include <vector>
 
@@ -36,6 +38,34 @@ TEST_F(ThreadSafeBufferTest, MultipleWritersSingleReader) {
   }
   for (auto i = 0; i < n_values; ++i) {
     buffer.read_next([&output_vector](int i) { output_vector.push_back(i); });
+  }
+
+  EXPECT_EQ(n_values, output_vector.size());
+  std::sort(output_vector.begin(), output_vector.end());
+  for (auto i = 0; auto const& x : output_vector) {
+    EXPECT_EQ(i++, x);
+  }
+}
+
+TEST_F(ThreadSafeBufferTest, SingleWriterMultipleReaders) {
+  auto readers = std::vector<std::jthread>{};
+  auto output_vector = std::vector<int>{};
+  auto output_mx = std::mutex{};
+
+  // need to start readers before writing on main thread
+  for (auto i = 0; i < n_values; ++i) {
+    readers.push_back(
+        std::jthread([this](auto read_func) { buffer.read_next(read_func); },
+                     [&output_vector, &output_mx](int i) {
+                       auto lock = std::lock_guard{output_mx};
+                       output_vector.push_back(i);
+                     }));
+  }
+  for (auto i = 0; i < n_values; ++i) {
+    buffer.write_next(i);
+  }
+  for (auto& r : readers) {
+    r.join();
   }
 
   EXPECT_EQ(n_values, output_vector.size());
