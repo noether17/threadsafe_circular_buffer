120e2b047a7c6ce5426fac28e3ec5d7f9f5ca468
Jonathan White <jawh@umich.edu>
2024-10-16 08:04:25 -0600

    Added mixed speeds test to ThreadSafeBuffer2Test.

--- src/test/ThreadSafeBuffer2Test.cpp
+++ src/test/ThreadSafeBuffer2Test.cpp
@@ -249,3 +249,74 @@ TEST_F(ThreadSafeBuffer2Test, MultipleWritersMultipleReadersSlowReads) {
     EXPECT_EQ(i++, x);
   }
 }
+
+TEST_F(ThreadSafeBuffer2Test, MultipleWritersMultipleReadersMixedSpeeds) {
+  auto writers = std::vector<std::jthread>{};
+  auto readers = std::vector<std::jthread>{};
+  auto output_vector = std::vector<int>{};
+  auto output_mx = std::mutex{};
+
+  // slow writes
+  for (auto i = 0; i < n_threads / 2; ++i) {
+    writers.push_back(std::jthread(
+        [this](int i) {
+          auto thread_offset = i * n_ops_per_thread;
+          for (auto j = 0; j < n_ops_per_thread; ++j) {
+            using namespace std::chrono_literals;
+            std::this_thread::sleep_for(1us);
+            buffer.write_next(thread_offset + j);
+          }
+        },
+        i));
+  }
+  // fast writes
+  for (auto i = n_threads / 2; i < n_threads; ++i) {
+    writers.push_back(std::jthread(
+        [this](int i) {
+          auto thread_offset = i * n_ops_per_thread;
+          for (auto j = 0; j < n_ops_per_thread; ++j) {
+            buffer.write_next(thread_offset + j);
+          }
+        },
+        i));
+  }
+  // slow reads
+  for (auto i = 0; i < n_threads / 2; ++i) {
+    readers.push_back(std::jthread(
+        [this](auto read_func) {
+          for (auto j = 0; j < n_ops_per_thread; ++j) {
+            buffer.read_next(read_func);
+          }
+        },
+        [&output_vector, &output_mx](int a) {
+          auto lock = std::lock_guard{output_mx};
+          using namespace std::chrono_literals;
+          std::this_thread::sleep_for(1us);
+          output_vector.push_back(a);
+        }));
+  }
+  // fast reads
+  for (auto i = n_threads / 2; i < n_threads; ++i) {
+    readers.push_back(std::jthread(
+        [this](auto read_func) {
+          for (auto j = 0; j < n_ops_per_thread; ++j) {
+            buffer.read_next(read_func);
+          }
+        },
+        [&output_vector, &output_mx](int a) {
+          auto lock = std::lock_guard{output_mx};
+          using namespace std::chrono_literals;
+          std::this_thread::sleep_for(1us);
+          output_vector.push_back(a);
+        }));
+  }
+  for (auto& r : readers) {
+    r.join();
+  }
+
+  EXPECT_EQ(n_values, output_vector.size());
+  std::sort(output_vector.begin(), output_vector.end());
+  for (auto i = 0; auto const& x : output_vector) {
+    EXPECT_EQ(i++, x);
+  }
+}
